00dcd71080800b9bb82c7bd2eccf50c2e8099a1b
Ivan Habunek <ivan@habunek.com>
2017-12-03 13:57:00 +0100

    Simplify core

--- src/aoc2017/core.clj
+++ src/aoc2017/core.clj
@@ -1,18 +1,8 @@
 (ns aoc2017.core)
 
-(defn get-ns-symbol [day]
-  (symbol
-    (str "aoc2017.day"
-      (format "%02d"
-        (Integer/parseInt day)))))
-
 (defn -main [day & args]
-  (if (re-matches #"^\d{1,2}$" day)
-    (let [ns-sym (get-ns-symbol day)]
-      (try
-        (require ns-sym)
-        (println (str "Advent of code 2017, day " day))
-        (apply (ns-resolve ns-sym 'main) args)
-        (catch Exception e
-          (println (.getMessage e)))))
-    (println "Invalid day given.")))
+  (let [ns-sym (symbol (str "aoc2017." day))]
+    (try
+      (require ns-sym)
+      (println "Advent of code 2017," day)
+      (apply (ns-resolve ns-sym 'main) args))))
