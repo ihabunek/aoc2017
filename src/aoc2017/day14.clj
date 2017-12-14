3c9f96c909692115e9e88738506ca297b1477288
Ivan Habunek <ivan@habunek.com>
2017-12-14 14:06:07 +0100

    Day 14, use set instead of map

--- src/aoc2017/day14.clj
+++ src/aoc2017/day14.clj
@@ -23,56 +23,39 @@
   (reduce +
     (map count-ones grid)))
 
-(defn grid-to-map
-  "Converts the grid (list of strings) into a map indexed by position [x y] and
-   having the value true if used, false if not."
-  [grid]
-  (into {}
+(defn grid-to-set [grid]
+  (into #{}
     (for [x (range (count (first grid)))
-          y (range (count grid))]
-      [[x y] (not= \0 (get (get grid y) x))])))
+          y (range (count grid))
+          :when (not= \0 (get (get grid y) x))]
+      [x y])))
 
 (defn neighbours [[x y]]
   [[(inc x) y] [x (inc y)]
    [(dec x) y] [x (dec y)]])
 
-(defn used-neighbours [grid-map pos]
-  (filter #(get grid-map %) (neighbours pos)))
+(defn used-neighbours [grid-set pos]
+  (filter #(contains? grid-set %) (neighbours pos)))
 
-(defn find-start
-  "Locates the first used position on the grid, or nil if none found."
-  [grid-map]
-  (first (first (filter val grid-map))))
-
-(defn find-region [grid-map pos]
-  (assert (not (nil? (get grid-map pos))))
-  (let [neighbours (used-neighbours grid-map pos)]
-    ; (println "find-region" pos neighbours)
+(defn find-region [grid-set pos]
+  (let [neighbours (used-neighbours grid-set pos)]
     (if (empty? neighbours)
       [pos]
       (concat [pos]
-        (mapcat #(find-region (assoc! grid-map pos false) %) neighbours)))))
-
-(defn clear-region [grid-map positions]
-  (loop [grid-map grid-map positions positions]
-    (if (empty? positions)
-      grid-map
-      (recur
-        (assoc grid-map (first positions) false)
-        (rest positions)))))
+        (mapcat #(find-region (disj! grid-set pos) %) neighbours)))))
 
 (defn count-regions [grid]
-  (loop [grid-map (grid-to-map grid)
+  (loop [grid-set (grid-to-set grid)
          region-count 0]
-    (let [pos (find-start grid-map)]
+    (let [pos (first grid-set)]
       (if (nil? pos)
         region-count
-        (let [region (find-region (transient grid-map) pos)]
+        (let [region (find-region (transient grid-set) pos)]
           (recur
-            (clear-region grid-map region)
+            (apply disj grid-set region)
             (inc region-count)))))))
 
 (defn main []
   (let [grid cached-grid]
-    (println "Part 1: There are" (time (count-used-squares grid)) "used squares")
+    ; (println "Part 1: There are" (time (count-used-squares grid)) "used squares")
     (println "Part 2: Found" (time (count-regions grid)) "regions")))
